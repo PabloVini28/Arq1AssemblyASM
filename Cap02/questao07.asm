6aaa726ab4b7342427da14d63f2eb0610f90e921
Pablo Vinícios da S.Araújo <128994831+PabloVini28@users.noreply.github.com>
2023-07-14 19:25:52 -0300

    Update questao07.asm

--- Cap02/questao07.asm
+++ Cap02/questao07.asm
@@ -2,7 +2,6 @@
 ;   Building Instructions:
 ;   ml /Cx /coff .\Main.asm /link /SUBSYSTEM:console /out:Main.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
 ;
-; Correção: 0,5
 
         .686
         .model  flat, c 
